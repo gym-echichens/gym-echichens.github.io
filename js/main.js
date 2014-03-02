87a251c1f849cb443a26d0f7721aa30e949f0623
gym-echichens <gym-echichens@bluewin.ch>
2014-03-02 23:56:37 +0100

    added bubble content: history and tariff

--- js/main.js
+++ js/main.js
@@ -8,12 +8,91 @@ var gym = {
 	},
 
 	/*------------- HTML -------------*/
-	frameHTML: '<div id="frame"><div class="row"><div id="history-box" class="col-md-6"></div><div id="tariff-box" class="col-md-6"></div></div> <div class="row"><div id="download-box" class="col-md-6"></div><div id="contact-box" class="col-md-6"></div></div> <div class="row"><div id="activity-box" class="col-md-12"></div></div></div>',
-	historyHTML: '<div class="box-center"><div class="bubble-topic el-center"><div class="bubble-title box-center"><div class="bubble-title-bottom el-center"></div><div class="bubble-title-top el-center"></div></div></div></div>',
-	tariffHTML: '<div class="box-center"><div class="bubble-topic el-center"><div class="bubble-title box-center"><div class="bubble-title-bottom el-center"></div><div class="bubble-title-top el-center"></div></div></div></div>',
-	downloadHTML: '<div class="box-center"><div class="bubble-topic el-center"><div class="bubble-title box-center"><div class="bubble-title-bottom el-center"></div><div class="bubble-title-top el-center"></div></div></div></div>',
-	contactHTML: '<div class="box-center"><div class="bubble-topic el-center"><div class="bubble-title box-center"><div class="bubble-title-bottom el-center"></div><div class="bubble-title-top el-center"></div></div></div></div>',
-	activityHTML: '<div class="box-center"><div class="bubble-title box-center"><div class="bubble-title-bottom el-center"></div><div class="bubble-title-top-activity el-center"></div></div></div>',
+	frameHTML: 		'<div id="frame">'+
+						'<div class="row">'+
+							'<div id="history-box" class="col-md-6"></div>'+
+							'<div id="tariff-box" class="col-md-6"></div>'+
+						'</div>'+
+						'<div class="row">'+
+							'<div id="download-box" class="col-md-6"></div>'+
+							'<div id="contact-box" class="col-md-6"></div>'+
+						'</div>'+
+						'<div class="row">'+
+							'<div id="activity-box" class="col-md-12"></div>'+
+						'</div>'+
+					'</div>',
+	
+	historyHTML: 	'<div class="box-center">'+
+						'<div class="bubble-topic el-center">'+
+							'<div class="bubble-title box-center">'+
+								'<div class="bubble-title-bottom el-center turquoise-bkg"></div>'+
+								'<div class="bubble-title-top el-center">'+
+									'<div class="title-theme"><%= dico.title %></div>'+
+								'</div>'+
+							'</div>'+
+							'<div class="topic-txt-center">'+
+								'<div><%= dico.paraph1 %>'+
+								'<div class="bold paraph">'+
+									'<%= dico.person1 %><br />'+
+									'<%= dico.person2 %><br />'+
+									'<%= dico.person3 %>'+
+								'</div>'+
+								'<div><%= dico.paraph2 %></div>'+
+							'</div>'+
+						'</div>'+
+					'</div>',
+	
+	tariffHTML: 	'<div class="box-center">'+
+						'<div class="bubble-topic el-center">'+
+							'<div class="bubble-title box-center">'+
+								'<div class="bubble-title-bottom el-center yellow-bkg"></div>'+
+								'<div class="bubble-title-top el-center">'+
+									'<div class="title-theme"><%= dico.title %></div>'+
+								'</div>'+
+							'</div>'+
+							'<div class="topic-txt">'+
+								'<div><%= dico.activity1.txt %><span class="right"><%= dico.activity1.cost %></span></div>'+
+								'<div><%= dico.activity2.txt %><span class="right"><%= dico.activity2.cost %></span></div>'+
+								'<div><%= dico.activity3.txt %><span class="right"><%= dico.activity3.cost %></span></div>'+
+								'<div><%= dico.activity4.txt %><span class="right"><%= dico.activity4.cost %></span></div>'+
+								'<div><%= dico.activity5.txt %><span class="right"><%= dico.activity5.cost %></span></div>'+
+								'<div><%= dico.activity6.txt %><span class="right"><%= dico.activity6.cost %></span></div>'+
+								'<div><%= dico.activity7.txt %><span class="right"><%= dico.activity7.cost %></span></div> <br />'+
+								'<div><%= dico.fee.txt %><span class="right"><%= dico.fee.cost %></span></div>'+
+							'</div>'+
+						'</div>'+
+					'</div>',
+	
+	downloadHTML: 	'<div class="box-center">'+
+						'<div class="bubble-topic el-center">'+
+							'<div class="bubble-title box-center">'+
+								'<div class="bubble-title-bottom el-center blue-bkg"></div>'+
+								'<div class="bubble-title-top el-center">'+
+									'<div class="title-theme"><%= dico.title %></div>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+					'</div>',
+	
+	contactHTML: 	'<div class="box-center">'+
+						'<div class="bubble-topic el-center">'+
+							'<div class="bubble-title box-center">'+
+								'<div class="bubble-title-bottom el-center orange-bkg"></div>'+
+								'<div class="bubble-title-top el-center">'+
+									'<div class="title-theme"><%= dico.title %></div>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+					'</div>',
+	
+	activityHTML: 	'<div class="box-center">' +
+						'<div class="bubble-title box-center"> ' +
+							'<div class="bubble-title-bottom el-center green-bkg"></div> ' +
+							'<div class="bubble-title-top-activity el-center"> ' +
+								'<div class="title-theme"><%= dico.title %></div> ' +
+							'</div> ' +
+						'</div> ' +
+					'</div>',
 
 	
 
